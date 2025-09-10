898b2a8aa8ffa2d31384cb6c8e229bcf4d528ef1
rchitaraj <140628397+rchitaraj@users.noreply.github.com>
2025-09-10 14:20:41 +0530

    Update DivisibilityCheck.java

--- src/DivisibilityCheck.java
+++ src/DivisibilityCheck.java
@@ -1,3 +1,4 @@
+//7. Write a program to check whether a number is divisible by 5 and 11 or not.//
 import java.util.Scanner;
 public class DivisibilityCheck {
     public static void main(String[] args) {
