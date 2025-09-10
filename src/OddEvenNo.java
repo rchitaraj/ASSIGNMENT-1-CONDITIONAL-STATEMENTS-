e335f46a601f7d315d9ef53f1b34e81e44804518
rchitaraj <140628397+rchitaraj@users.noreply.github.com>
2025-09-10 14:27:30 +0530

    Update OddEvenNo.java

--- src/OddEvenNo.java
+++ src/OddEvenNo.java
@@ -1,6 +1,5 @@
+//2.Write a Java program to check if a number entered by the user is even or odd.//
 import java.util.Scanner;
-//Write a Java program to check if a number entered by the user is even or odd.
-//Use the modulus operator (%) and an if-else condition.
 public class OddEvenNo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
